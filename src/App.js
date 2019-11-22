ce42095dd6611a985a213a0eb03ca4ad0741cbef
Alain Armand <aarmand.inbox@users.noreply.gmail.com>
2019-11-21 20:04:12 -0500

    classes->hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React, { Component } from "react";
-import { useEffect, useReducer } from 'react'
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import logo from "./logo.svg";
-import Amplify from "@aws-amplify/core";
-import "@aws-amplify/pubsub";
-import API, { graphqlOperation } from "@aws-amplify/api";
-import aws_exports from "./aws-exports";
+import API, { graphqlOperation } from '@aws-amplify/api';
+import Amplify from '@aws-amplify/core';
+import '@aws-amplify/pubsub';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { default as React, useEffect, useState } from 'react';
+import './App.css';
+import aws_exports from './aws-exports';
+import logo from './logo.svg';
 Amplify.configure(aws_exports);
 
 const createMessage = `mutation createMessage($message: String!){
@@ -26,89 +25,89 @@ const onCreateMessage = `subscription onCreateMessage {
     }
 }`;
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      message: "",
-      value: "",
-      display: false
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+function App() {
+	// Declare a new state variable, which we'll call "count"
+	const [
+		message,
+		setMessage
+	] = useState('');
+	const [
+		value,
+		setValue
+	] = useState('');
+	const [
+		display,
+		setDisplay
+	] = useState(false);
 
-  async componentDidMount() {
-    this.subscription = API.graphql(
-      graphqlOperation(onCreateMessage)
-    ).subscribe({
-      next: event => {
-        if (event){
-          console.log("Subscription: " + JSON.stringify(event.value.data, null, 2));
-          console.log("EVENT: " + JSON.stringify(event, null, 2));
-          this.setState({ display: true });
-          this.setState({ message: event.value.data.onCreateMessage.message });
-        }
-      }
-    });
-  }
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		event.stopPropagation();
+		const message = {
+			id: '',
+			message: value,
+			createdAt: ''
+		};
+		console.log('Message: ' + message);
+		const mutation = API.graphql(graphqlOperation(createMessage, message));
+		console.log('Mutation: ' + JSON.stringify(mutation.data, null, 2));
+	};
 
-  handleChange(event) {
-    this.setState({ value: event.target.value });
-  }
+	useEffect(() => {
+		const subscription = API.graphql(graphqlOperation(onCreateMessage)).subscribe({
+			next: (event) => {
+				if (event) {
+					console.log('Subscription: ' + JSON.stringify(event.value.data, null, 2));
+					console.log('EVENT: ' + JSON.stringify(event, null, 2));
+					setDisplay(true);
+					let message = event.value.data.onCreateMessage.message;
+					setMessage(message);
+				}
+			}
+		});
 
-  async handleSubmit(event) {
-    event.preventDefault();
-    event.stopPropagation();
-    const message = {
-      id: "",
-      message: this.state.value,
-      createdAt: ""
-    };
-    const mutation = await API.graphql(
-      graphqlOperation(createMessage, message)
-    );
-    console.log("Mutation: " + JSON.stringify(mutation.data, null, 2));
-  }
+		return () => subscription.unsubscribe();
+	}, []);
 
-  render() {
-    return (
-      <div className="App">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div className="jumbotron jumbotron-fluid p-0">
-          <h2 className="center">Broadcaster</h2>
-        </div>
-        <br />
-        <div className="container">
-          <form onSubmit={this.handleSubmit}>
-            <div className="form-group">
-              <input
-                className="form-control form-control-lg"
-                type="text"
-                value={this.state.value}
-                onChange={this.handleChange}
-              />
-            </div>
-            <input
-              type="submit"
-              value="Submit"
-              className="btn btn-primary"
-            />
-          </form>
-        </div>
-        <br />
-        {this.state.display ? (
-          <div className="container">
-            <div className="card bg-success">
-              <h3 className="card-text text-white p-2">
-                {this.state.message}
-              </h3>
-            </div>
-          </div>
-        ) : null}
-      </div>
-    );
-  }
+	const handleChange = (e) => {
+		console.log(e.target.value);
+		// 	const value = e.target.value;
+		setValue(e.target.value);
+	};
+	// function handleChange(event) {
+	// 	console.log(event);
+	// 	const value = event.target.value;
+	// 	setValue(value);
+	// }
+	return (
+		<div className="App">
+			<img src={logo} className="App-logo" alt="logo" />
+			<div className="jumbotron jumbotron-fluid p-0">
+				<h2 className="center">Broadcaster</h2>
+			</div>
+			<br />
+			<div className="container">
+				<form onSubmit={handleSubmit}>
+					<div className="form-group">
+						<input
+							className="form-control form-control-lg"
+							type="text"
+							value={value}
+							onChange={handleChange}
+						/>
+					</div>
+					<input type="submit" value="Submit" className="btn btn-primary" />
+				</form>
+			</div>
+			<br />
+			{
+				display ? <div className="container">
+					<div className="card bg-success">
+						<h3 className="card-text text-white p-2">{message}</h3>
+					</div>
+				</div> :
+				null}
+		</div>
+	);
 }
-
 export default App;
